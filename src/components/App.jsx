e026849c2f523be46e6b22a65517e5fb098a939f
deelcoding <deelcoding@outlook.com>
2024-11-13 20:10:26 -0500

    Import of jsx files to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import reactLogo from '../images/react.svg'
-import viteLogo from '/vite.svg'
-import '../blocks/App.css'
+import { useState } from "react";
+import reactLogo from "../images/react.svg";
+import viteLogo from "/vite.svg";
+import "../blocks/App.css";
+import Header from "./Header.jsx";
+import Main from "./Main.jsx";
+import Footer from "./Footer.jsx";
+import ModalWithForm from "./ModalWithForm.jsx";
+import ItemModal from "./ItemModal.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
+      <Header />
+      <Main />
+      <Footer />
+      <ModalWithForm />
+      <ItemModal />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
