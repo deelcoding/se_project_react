7aa0a6a8cdae4fdb4d9bd4e13f75f433234a07d2
deelcoding <deelcoding@outlook.com>
2024-11-21 23:59:09 -0500

    Modal work

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -2,32 +2,6 @@ import { useEffect } from "react";
 import "./ItemModal.css";
 
 function ItemModal({ activeModal, onClose, card }) {
-  // Effect to handle ESC key press and outside click
-  useEffect(() => {
-    const handleEscKey = (event) => {
-      if (event.key === "Escape") {
-        onClose(); // Close the modal if ESC key is pressed
-      }
-    };
-
-    const handleOutsideClick = (event) => {
-      const modalContent = document.querySelector(".modal__content");
-      if (modalContent && !modalContent.contains(event.target)) {
-        onClose(); // Close the modal if clicking outside modal content
-      }
-    };
-
-    // Add event listeners
-    window.addEventListener("keydown", handleEscKey);
-    window.addEventListener("click", handleOutsideClick);
-
-    // Cleanup event listeners on component unmount or when modal closes
-    return () => {
-      window.removeEventListener("keydown", handleEscKey);
-      window.removeEventListener("click", handleOutsideClick);
-    };
-  }, [onClose]);
-
   return (
     <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
       <div className="modal__content modal__content_type_image">
