6d5113bf9738c103bc2e8d37422d62c63caacc4c
deelcoding <deelcoding@outlook.com>
2024-12-04 19:42:51 -0500

    Project 11 Started

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,41 @@
 import headerLogo from "../../images/headerLogo.png";
 import userAvatar from "../../images/user_avatar.png";
 import "./Header.css";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+import { useState } from "react";
 
-
-function Header({onAddGarment, weatherData}) {
-
+function Header({ onAddGarment, weatherData }) {
   const currentDate = new Date().toLocaleString("default", {
     month: "long",
     day: "numeric",
   });
 
+  // const [value, setValue] = useState(false);
+
   return (
     <header className="header">
-      <img
-        src={headerLogo}
-        alt="App Logo"
-        className="header__logo"
-      />
-      <p className="header__date-and-location">
-        {currentDate}, {weatherData.city}
-      </p>
-      <button onClick={onAddGarment} type="button" className="header__button">
-        <span className="header__button-text">+ Add clothes</span>
-      </button>
+      <div className="header__logo-container">
+        <img
+          src={headerLogo}
+          alt="App Logo"
+          className="header__logo"
+        />
+        <p className="header__date-and-location">
+          {currentDate}, {weatherData.city}
+        </p>
+      </div>
       <div className="header__user-container">
+        <ToggleSwitch
+        // isOn={value}
+        // onColor="#EF476F"
+        // handleToggle={() => setValue(!value)}
+        />
+        <button
+          onClick={onAddGarment}
+          type="button"
+          className="header__button">
+          <span className="header__button-text">+ Add clothes</span>
+        </button>
         <p className="header__username">Terrence Tegegne</p>
         <img
           src={userAvatar}
