74e5e4429b89874fa9bffd7cd475e75c09a6d8a0
Dacotah <deelcoding@outlook.com>
2025-04-01 19:29:59 -0400

    Sprint 15 - Deploying Frontend to server

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.wtwrdeelcoding.crabdance.com"
+    : "http://localhost:3001";
 
 const token = localStorage.getItem("jwt");
 
